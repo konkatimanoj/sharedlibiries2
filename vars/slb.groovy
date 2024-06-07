23dffdf3f2eda6c7e5e65905add19a81f1e235b9
konkatimanoj <162788980+konkatimanoj@users.noreply.github.com>
2024-06-07 10:53:39 +0530

    Update slb.groovy

--- vars/slb.groovy
+++ vars/slb.groovy
@@ -10,3 +10,7 @@ def gitdeploy(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
+def gittesting(namejob)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${namejob}/testing.jar"
+}
